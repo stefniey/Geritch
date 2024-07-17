ca3b57059ec2476bf7b8bcd576c714cc63ff8c1d
stefniey <stephaniealor7@gmail.com>
2024-07-17 10:13:37 -0700

    Initial commit

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { SubHeading} from '../../components'
 import { images} from '../../constants'
@@ -24,5 +25,41 @@ const FindUs = () => (
 
   </div>
 );
+=======
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './FindUs.css';
+
+const FindUs = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true, 
+    });
+  }, []);
+
+  return (
+    <div className='app__bg app__wrapper section__padding' id='contact'>
+      <div className="app__wrapper_info">
+        <SubHeading title='Contact' />
+        <h1 className='headtext__cormorant' data-aos="fade-left">Find Us</h1>
+        <div className="app__wrapper-content" data-aos="fade-up">
+          <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+          <p className="p__cormorant">Opening Hours</p>
+          <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
+          <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+          <button type="button" className="custom__button">Visit Us</button>
+        </div>
+      </div>
+      <div className="app__wrapper_img" data-aos="fade-right">
+        <img src={images.findus} alt="findus" />
+      </div>
+    </div>
+  );
+}
+>>>>>>> 410c1d9 (Your commit message)
 
 export default FindUs;
